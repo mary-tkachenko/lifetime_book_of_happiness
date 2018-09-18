10e7fb8289fcc3649e497c7bd3225ddf51426019
Mary <l.skyling@gmail.com>
2018-09-17 22:18:44 -0400

    setting custom fields for the devise registration and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,5 +34,30 @@ class UsersController < ApplicationController
           format.json { head :no_content }
         end
       end
+
+      def create
+        @user = User.create(user_params)
+
+        respond_to do |format|
+          if @user.save
+            format.html { redirect_to @user, notice: 'User was successfully created.' }
+            format.json { render :show, status: :created, location: @user }
+          else
+            format.html { render :new }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+      end
+    
+      ...
+    
+      private
+        def user_params
+          # It's mandatory to specify the nested attributes that should be permitted.
+          # If you use `permit` with just the key that points to the nested attributes hash,
+          # it will return an empty hash.
+          params.require(:user).permit(:nickname, :email)
+        end
     
 end
